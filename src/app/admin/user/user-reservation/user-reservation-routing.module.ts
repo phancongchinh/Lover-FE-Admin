24b38018578917287292961ac42e27c81a69f0e0
chinhpc <phancongchinh@gmail.com>
2021-12-09 12:15:28 +0700

    Sidebar link to user's reservation

--- src/app/admin/user/user-reservation/user-reservation-routing.module.ts
+++ src/app/admin/user/user-reservation/user-reservation-routing.module.ts
@@ -1,11 +1,23 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {UserReservationListComponent} from './user-reservation-list/user-reservation-list.component';
+import {UserReservationViewComponent} from './user-reservation-view/user-reservation-view.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: UserReservationListComponent
+  },
+  {
+    path: 'view/:id',
+    component: UserReservationViewComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
-export class UserReservationRoutingModule { }
+export class UserReservationRoutingModule {
+}
